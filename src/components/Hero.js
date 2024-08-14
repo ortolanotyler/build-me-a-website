a3dea698a6be1a26eecf873846fe46cdd4aeddd8
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-08-14 09:46:32 -0400

    43

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -35,29 +35,21 @@ const ParentContainer = styled('div')({
   justifyContent: 'center',
   alignItems: 'center',
   backgroundColor: '#F4E1D2', // Ensure background color covers the entire viewport
+  padding: '125px 50px', // Add some padding to the container
+ 
 });
 
 const HeroContainer = styled(Grid)(({ theme }) => ({
   backgroundColor: '#F4E1D2',
-  minHeight: '100vh',
-  maxWidth: '80%',
+  maxWidth: '1000px', // Limit the maximum width of the container
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'space-between',
   alignItems: 'center',
-  margin: ' 0', // Center horizontally
-  padding: 'calc(100px + 30px)', // Adjust padding to account for the fixed header
-
-  paddingTop: 'calc(100px + 30px)', // Adjust padding to account for the fixed header
-  [theme.breakpoints.down(1000)]: {
+  padding: '100px', // Add some padding inside the container
+  [theme.breakpoints.down('md')]: {
     flexDirection: 'column',
-    marginRight: '40px',
-    padding: '100px 50px', // Adjust padding for medium screens
-    minHeight: '100vh',
-  },
-  [theme.breakpoints.down(800)]: {
-    flexDirection: 'column',
-    padding: '100px 30px',
-    minHeight: '100vh',
+    textAlign: 'center',
+    padding: '75px 25px', // Adjust padding for medium screens
   },
 }));
 
@@ -65,16 +57,12 @@ const TextContainer = styled(Grid)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
-  textAlign: 'center', // Center text horizontally
-  
+  textAlign: 'left', // Align text to the left
   animation: `${slideInLeft} 1s ease-out`, // Apply the left slide-in animation
-  [theme.breakpoints.down(1300)]: {
-    alignItems: 'center', // Ensure content is centered
-    marginTop: '20px', // Adjust top margin for medium screens
-  },
-  [theme.breakpoints.down(1100)]: {
-    alignItems: 'center', // Ensure content is centered
-    marginTop: '20px', // Adjust top margin for small screens
+  marginBottom: '20px', // Add some margin at the bottom
+  [theme.breakpoints.down('md')]: {
+    textAlign: 'center', // Center text on smaller screens
+    marginBottom: '20px', // Keep some margin at the bottom
   },
 }));
 
@@ -83,48 +71,35 @@ const Title = styled('h1')(({ theme }) => ({
   fontSize: '2.5rem',
   color: '#3A3A3A',
   fontWeight: 'normal',
-  marginBottom: '10px',
-  padding: '10px ',
-  [theme.breakpoints.down(1000)]: {
-    fontSize: '2.25rem',
-    marginBottom: '25px',
-  },
-  [theme.breakpoints.down(800)]: {
-    fontSize: '2rem',
-    marginBottom: '10px',
+  marginBottom: '20px', // Add some margin at the bottom
+  [theme.breakpoints.down('md')]: {
+    fontSize: '2rem', // Adjust font size for medium screens
   },
 }));
 
 const StyledButton = styled(Button)(({ theme }) => ({
   fontFamily: 'Nunito, sans-serif',
-  font: 'inherit',
   backgroundColor: '#3a3a3a',
   color: '#F4E1D2',
-  textAlign: 'center',
-  maxWidth: '150px',
+  width: '180px', // Set a fixed width for the button
   borderRadius: '10px',
   fontSize: '1rem',
   textTransform: 'none', // Prevent text from being capitalized
   boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)', // Add a shadow for more depth
-  fontWeight: 'normal',
   transition: 'transform 0.25s ease, box-shadow 0.25s ease',
   cursor: 'pointer',
-  alignSelf: 'center', // Center button on all screen sizes
+  alignSelf: 'center', // Center the button horizontally
   '&:hover': {
     transform: 'scale(1.05)', // Slightly increase size on hover
+    backgroundColor: '#F2784B', // Change background color on hover
     boxShadow: '0 4px 12px rgba(0, 0, 0, 0.3)', // Add a shadow for more depth
-    backgroundColor: '#F2784B', // Ensure background color goes orange
-    color: '#F4E1D2', // Ensure text color stays beige
   },
   '&:active': {
     transform: 'scale(0.95)', // Slightly squish on click
-    boxShadow: '0 2px 6px rgba(0, 0, 0, 0.2)', // Reduce shadow on click
     backgroundColor: '#3A3A3A', // Ensure background color stays black
-    color: '#3a3a3a', // Ensure text color stays beige
   },
-  [theme.breakpoints.down(800)]: {
-    fontSize: '0.875rem', // Make button font smaller on small screens
-    width: '130px', // Reduce button width on small screens
+  [theme.breakpoints.down('md')]: {
+    marginTop: '10px', // Add some spacing above the button on small screens
   },
 }));
 
@@ -133,25 +108,14 @@ const ImageContainer = styled(Grid)(({ theme }) => ({
   justifyContent: 'center',
   alignItems: 'center',
   animation: `${slideInRight} 1s ease-out`, // Apply the right slide-in animation
-  [theme.breakpoints.down(1300)]: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  
-  },
-  [theme.breakpoints.down(1100)]: {
-    width: '100%',
-    
+  [theme.breakpoints.up('md')]: {
+    width: '50%', // Limit the image container width on large screens
+    alignSelf: 'flex-end', // Align image to the right
   },
   '& img': {
-    width: '80%',
+    width: '100%',
+    maxHeight: '500px', // Limit the maximum height of the image
     borderRadius: '10px',
-    [theme.breakpoints.up('md')]: {
-      width: '75%', // Reduce image size by 25% on large screens
-      alignSelf: 'flex-start', // Align image to the left
-    },
-    [theme.breakpoints.down(800)]: {
-      marginLeft: '1px', // Add margin to the left for small screens
-    },
   },
 }));
 
@@ -258,7 +222,7 @@ const Hero = () => {
             />
             <Button 
               variant="contained" 
-              style={{ borderRadius: '10px',backgroundColor: '#3A3A3A', color: '#f5f5dc', marginTop: '20px' }} 
+              style={{ borderRadius: '10px', backgroundColor: '#3A3A3A', color: '#f5f5dc', marginTop: '20px' }} 
               fullWidth
               onClick={handleClosePopup} // For now, just close the popup on submit
             >
@@ -272,3 +236,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
